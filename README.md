b4efce595b79ea9237d44de45fffe92882c790bb
al <usernamehw@gmail.com>
2018-09-23 14:20:14 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# do-not-copy-empty README
+
+Prevents accidental copying (<kbd>Ctrl</kbd>/<kbd>Cmd</kbd>+<kbd>C</kbd>) of empty selection (empty line or whitespace/tabs) when there's no selection and only 1 cursor.
---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.doNotCopyEmpty', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+
+        if (editor.selections.length === 1) {
+            const activeLine = editor.document.lineAt(editor.selections[0].active);
+            if (activeLine.isEmptyOrWhitespace) return;
+        }
+
+        vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
