6c7f677d0ea65244a394e5c9b87228eab0fbb495
al <usernamehw@gmail.com>
2019-07-23 23:55:03 +0300

    💄

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# DEPRECATED
-
-This functionality will be included in the next vscode release **1.30.0**.
-
----
-
-Prevents accidental copying (<kbd>Ctrl</kbd>/<kbd>Cmd</kbd>+<kbd>C</kbd>) of empty selection (empty line or whitespace/tabs) when there's no selection and only 1 cursor.
+Prevents accidental copying ( <kbd>Ctrl</kbd> / <kbd>Cmd</kbd>+<kbd>C</kbd> ) of empty selection (empty line or whitespace/tabs) when there's no selection and only 1 cursor.
--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,19 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.doNotCopyEmpty', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return;
+	let disposable = vscode.commands.registerCommand('extension.doNotCopyEmpty', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
 
-        if (editor.selections.length === 1) {
-            const activeLine = editor.document.lineAt(editor.selections[0].active);
-            if (activeLine.isEmptyOrWhitespace) return;
-        }
+		if (editor.selections.length === 1) {
+			const activeLine = editor.document.lineAt(editor.selections[0].active);
+			if (activeLine.isEmptyOrWhitespace) return;
+		}
 
-        vscode.commands.executeCommand('editor.action.clipboardCopyAction');
-    });
+		vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+	});
 
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
 export function deactivate() { }
