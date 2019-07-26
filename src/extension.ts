e30a4e611ebd068831c3322718f11a8c080ee582
al <usernamehw@gmail.com>
2019-07-26 10:16:43 +0300

    💄

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,13 @@
 'use strict';
 import * as vscode from 'vscode';
-
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.doNotCopyEmpty', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) return;
-
+	let disposable = vscode.commands.registerTextEditorCommand('extension.doNotCopyEmpty', (editor) => {
 		if (editor.selections.length === 1) {
 			const activeLine = editor.document.lineAt(editor.selections[0].active);
 			if (activeLine.isEmptyOrWhitespace) return;
 		}
-
 		vscode.commands.executeCommand('editor.action.clipboardCopyAction');
 	});
-
 	context.subscriptions.push(disposable);
 }
-
 export function deactivate() { }
